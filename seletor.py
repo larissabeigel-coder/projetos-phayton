b6008b0a77ed4d51606a120ebd1f8315e8c13752
larissabeigel-coder <larissabeigel@gmail.com>
2026-05-04 09:52:24 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#--seletor de caracteres--
+letra = input("digite a letra")
+if letra == "A":
+  print("você escolheu a letra A")
+elif letra == "B":
+  print(você escolheu a letra "B":
+else:
+  print("Essa opção não está disponível")
